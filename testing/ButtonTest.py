ecb21fd50b849e7d2247c329e5e1997b5d39f888
adiravishankara <adiravishankara@gmail.com>
2021-09-20 15:29:23 -0700

    go

--- testing/ButtonTest.py
+++ testing/ButtonTest.py
@@ -1,13 +1,19 @@
-import Adafruit_ADS1x15 as adc
-#are you working b
+from adafruit_ads1x15 import ads1115 as adc
+import board
+import busio
+from adafruit_ads1x15.analog_in import AnalogIn as ai
+i2c = busio.I2C(board.SCL, board.SDA)
+
+adc1 = adc.ADS1115(i2c)
+
 def main():
 
-    channel = 0
-
-    if adc.read_adc(channel) < 150:
-        print("button is pushed")
-    else:
-        print("button is not pushed")
+    channel = ai(adc1, adc.P0)
+    print(channel.value)
+    #if adafruit_ads1x15.single_ended.ADS1x15_SingleEnded.read_adc(channel) < 150:
+#         print("button is pushed")
+#     else:
+#         print("button is not pushed")
 
 if __name__ == "__main__":
     main()
