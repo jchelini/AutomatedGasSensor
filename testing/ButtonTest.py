d69bc9f178b69ce4a1eb93dce183eac8bc2c8f10
johan <johanna.chelini@gmail.com>
2021-11-01 12:32:46 -0700

    gui

--- testing/ButtonTest.py
+++ testing/ButtonTest.py
@@ -2,9 +2,13 @@ from adafruit_ads1x15 import ads1115 as adc
 import board
 import busio
 from adafruit_ads1x15.analog_in import AnalogIn as ai
+import qwiic_relay as qr
+import time
 i2c = busio.I2C(board.SCL, board.SDA)
 
 adc1 = adc.ADS1115(i2c)
+
+relay = qr.QwiicRelay(0x18)
 
 def main():
 
@@ -15,9 +19,10 @@ def main():
     else:
         print("button is pushed")
 
-    relay = QwiicRelay(0x18)
+    time.sleep(7)
     relay.set_relay_off()
-
+    time.sleep(4)
+    relay.set_relay_on()
 
 if __name__ == "__main__":
     main()
